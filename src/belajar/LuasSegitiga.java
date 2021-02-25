452c1287e909410da57bb93715987b98f79a3965
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2021-02-25 18:36:54 +0700

    Program 1

---
+++ src/belajar/LuasSegitiga.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class LuasSegitiga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //object
+        New object = new New();
+        //untuk menjalankan method New
+        object.New(36 , 12);
+        //untuk menjalankan method hitung
+        object.hitung();
+    }
+    
+}
